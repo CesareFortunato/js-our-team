73c19c45a7c1717638077b3011b67e2dfd735e60
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-16 14:48:31 +0100

    feat: css + html + logic to populate html with cards in js

--- script.js
+++ script.js
@@ -36,3 +36,40 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+
+// setup variables
+
+
+
+const outputContainer = document.getElementById("cards-container");
+
+let outputCards = "";
+
+populateCards()
+
+
+function populateCards(){
+
+for (let index = 0; index < teamMembers.length; index++) {
+  const memberTeam = teamMembers[index];
+
+  outputCards += `<div class="card mb-3 col-4" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${memberTeam.img}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8 card-text-container">
+                        <div class="card-body">
+                            <h5 class="card-title">${memberTeam.name}</h5>
+                            <p class="card-text">${memberTeam.role}</p>
+                            <a href="" class="mail-text">${memberTeam.email}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+
+}
+
+outputContainer.innerHTML = outputCards;
+}
