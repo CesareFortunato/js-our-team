20d5ef4c4a629a883de2936d8e0aab2e71e91e17
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-16 15:19:34 +0100

    feat: fixed function + compact system

--- script.js
+++ script.js
@@ -57,47 +57,44 @@ const img = document.getElementById("img-field");
 
 // SYSTEM
 
+for (let index = 0; index < teamMembers.length; index++) {
+  const memberTeam = teamMembers[index]
 
-populateCards()
+  outputCards += populateCards(memberTeam);
+
+}
+
 
 
 
 // FUNCTION
 
-function populateCards(){
-
-for (let index = 0; index < teamMembers.length; index++) {
-  const memberTeam = teamMembers[index];
-
-  // destructuring obj memberTeam
-
-  const {name, img, role  } = memberTeam;
+function populateCards(memberObj){
 
   // create a <div> structure for every obj in teamMembers array
 
-  outputCards += `<div class="card mb-3 col-4" style="max-width: 540px;">
+  const card = `<div class="card mb-3 col-4" style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="${img}" class="img-fluid rounded-start" alt="${name}">
+                        <img src="${memberObj.img}" class="img-fluid rounded-start" alt="${memberObj.name}">
                     </div>
                     <div class="col-md-8 card-text-container">
                         <div class="card-body">
-                            <h5 class="card-title">${name}</h5>
-                            <p class="card-text">${role}</p>
-                            <a href="" class="mail-text">${email}</a>
+                            <h5 class="card-title">${memberObj.name}</h5>
+                            <p class="card-text">${memberObj.role}</p>
+                            <a href="" class="mail-text">${memberObj.email}</a>
                         </div>
                     </div>
                 </div>
             </div>`
+
+            return card
 
 }
 
 // put every card inside the outputContainer <div>
 
 outputContainer.innerHTML = outputCards;
-}
 
 
-function newMember() {
-  
-}
+
